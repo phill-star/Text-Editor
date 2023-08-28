9ad07849cba02748955dfeda460eb42bfeab27c6
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-08-28 09:27:11 -0600

    addedButinstall

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -8,7 +8,18 @@ window.addEventListener('beforeinstallprompt', (event) => {
     installBtn.classList.toggle('hidden', false);
 });
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
 
+    if (!promptEvent) {
+        return;
+    }
+
+    promptEvent.prompt();
+
+    window.deferredPrompt = null;
+
+    installBtn.classList.toggle('hidden', true);
+});
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {});
