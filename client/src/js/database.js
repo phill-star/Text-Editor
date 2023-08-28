4cff20957800cb0771724411618b4e5251664ddd
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-08-28 13:51:42 -0600

    addedPutRequestFunction

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,15 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, value) => {
+  console.log('PUT request to update the jateDB');
+  const jateDb = await openDB('jate', 1); 
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const objStore = tx.objectStore('jate');
+  const req = objStore.put({ id: id, value: value })
+  const res = await req;
+  console.log('data saved to the jateDB', res);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
