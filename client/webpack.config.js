0d0492659e32ccd2dcbd823e0b954f8600184cca
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-08-28 09:06:50 -0600

    addedAssetcaching

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -49,4 +49,26 @@ module.exports = () => {
         ],
       }),
     ],
-  }};
+
+    module: {
+      // TODO: Add CSS loaders and babel to webpack.
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+            },
+          },
+        },
+      ],
+    },
+  };
+};
